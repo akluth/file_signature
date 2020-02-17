4b7c8b239153009f7b879d857e130970820cbb69
Alexander Kluth <clooze@pm.me>
2020-02-17 15:12:49 +0100

    Restructure methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,31 @@
 use std::fs::File;
 use std::io::Read;
 
+pub struct Type;
+
+impl Type {
+    pub const GZIP: [u8; 3] = [0x1f, 0x8b, 0x8];
+    pub const BZIP2: [u8; 3] = [0x42, 0x5a, 0x68];
+    pub const UNKNOWN: bool = false;
+}
+
 pub struct FileSignature;
 
 impl FileSignature {
-    pub const GZIP: [u8; 3] = [0x1f, 0x8b, 0x8];
-    pub const BZIP2: [u8; 3] = [0x42, 0x5a, 0x68];
 
-    pub fn get(path: &str) -> Option<&[u8]> {
+    fn get_bytes(path: &str) -> [u8; 3] {
         let mut file = File::open(path).unwrap();
         let mut buffer = [0u8; 3];
         file.read(&mut buffer).unwrap();
 
-        if buffer == FileSignature::GZIP {
-            Some(&FileSignature::GZIP)
-        } else if buffer == FileSignature::BZIP2 {
-            Some(&FileSignature::BZIP2)
-        } else {
-            None
-        }
+        buffer
     }
 
-    pub fn is_a(source_bytes: [u8; 3], bytes: [u8; 3]) -> bool {
+    pub fn get(path: &str) -> [u8; 3] {
+        FileSignature::get_bytes(path)
+    }
+
+    pub fn compare_bytes(source_bytes: [u8; 3], bytes: [u8; 3]) -> bool {
         if source_bytes == bytes {
             true
         } else {
@@ -36,11 +40,11 @@ mod tests {
 
     #[test]
     fn it_is_a_gzip_file() {
-        assert_eq!(FileSignature::is_a([0x1f, 0x8b, 0x8], FileSignature::GZIP), true);
+        assert_eq!(FileSignature::compare_bytes([0x1f, 0x8b, 0x8], Type::GZIP), true);
     }
 
     #[test]
     fn it_is_not_a_gzip_file() {
-        assert_eq!(FileSignature::is_a([0x1f, 0x9a, 0x8], FileSignature::GZIP), false);
+        assert_eq!(FileSignature::compare_bytes([0x1f, 0x9a, 0x8], Type::GZIP), false);
     }
 }
